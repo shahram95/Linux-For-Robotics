87427204b86552f249efd68f9d5be3eab391279c
shahram95 <shahramnajam.neduet@gmail.com>
2024-12-26 03:06:19 -0500

    Added bash script for controlling bb8

---
+++ catkin_ws/src/demo_files/demo.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+ARG1=$1
+
+if [ $ARG1 == 'forward' ]; then
+    rostopic pub /cmd_vel geometry_msgs/Twist "linear:
+  x: -0.1
+  y: 0.0
+  z: 0.0
+angular:
+  x: 0.0
+  y: 0.0
+  z: 0.0"
+
+elif [ $ARG1 == 'rotate' ]; then
+    rostopic pub /cmd_vel geometry_msgs/Twist "linear:
+  x: 0.0
+  y: 0.0
+  z: 0.0
+angular:
+  x: 0.0
+  y: 0.0
+  z: 0.2"
+
+elif [ $ARG1 == 'stop' ]; then
+    rostopic pub /cmd_vel geometry_msgs/Twist "linear:
+  x: 0.0
+  y: 0.0
+  z: 0.0
+angular:
+  x: 0.0
+  y: 0.0
+  z: 0.0"
+fi
